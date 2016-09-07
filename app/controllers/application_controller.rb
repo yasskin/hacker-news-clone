ee2508c1215306dafe44c5f5efc682a5e1f49092
Dylan Barnard and Noah Yasskin <dylan+noah@>
2016-09-07 16:42:35 -0700

    Create helper to find host in post link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  require 'uri'
+
+  def find_root(link)
+    URI.parse(link).host
+  end
+  helper_method :find_root
+end
+
+
+def hostname(v)
 end
