09e8c80c3a43b0c9fc3c78f3715251ebab90470a
Dylan Barnard and Noah Yasskin <dylan+noah@>
2016-09-07 11:18:12 -0700

    Users can add posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,21 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+private
+  def post_params
+    params.require(:post).permit(:name, :link)
+  end
 end
